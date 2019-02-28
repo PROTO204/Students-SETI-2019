c6ed6df14fa481b0e0b87c0bc357f1038229df8b
ihaur <haurimane@gmail.com>
2019-02-28 11:51:47 +0000

    Application_Imane_py

---
+++ Application_Imane_py/listName.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+# coding : utf8 
+
+list = ["imane","imad","brice"]
+
+print ("Hello word")
+name = input("qui voulez vous?")
+if name in list:
+	print ("eleve present")
+else: 
+	print ("pas de "+name+" present")
+
